9b8bcae5953a68d8891ad74b684ca3e986fdb0ba
sharada0417 <sharada.kanchana417@gmail.com>
2024-12-26 02:46:28 +0530

    Update submit button and Update Delete button functionalities

--- script.js
+++ script.js
@@ -59,3 +59,62 @@ const formatTime = () =>{
     timeElement.textContent = newhour+" : "+minite+" "+lap;
 }
 formatTime();
+
+const todoCreateButton = document.querySelector(".todo__create__button");
+const todoInput = document.querySelector(".todo__input");
+const todoContainer = document.querySelector(".todo__container");
+
+const todoValues = [];
+let todoElements = [];
+
+todoCreateButton.addEventListener("click", () => {
+    const value = todoInput.value;
+    if (value === "") {
+      return;
+    }
+    todoValues.push(value);
+    console.log(todoValues);
+    todoInput.value = "";
+  
+    renderTodos();
+  });
+  
+  const renderTodos = () => {
+    todoElements = todoValues.map((val, index) => {
+      return `<div class="todo__item" data-index="${index}">
+        <div class="todo__item__left">
+          <input type="checkbox" id="completed" name="completed" />
+          <span>${val}</span>
+        </div>
+        <div class="todo__item__right">
+          <svg
+            class="todo__delete__button"
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="red"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M3 6h18" />
+            <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+            <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+          </svg>
+        </div>
+      </div>`;
+    });
+    todoContainer.innerHTML = todoElements.join(" ");
+  };
+  
+  todoContainer.addEventListener("click", (e) => {
+    if (e.target.closest(".todo__delete__button")) {
+      const todoItem = e.target.closest(".todo__item");
+      const index = todoItem.dataset.index;
+      todoValues.splice(index, 1); 
+      renderTodos(); 
+    }
+  });
+  
